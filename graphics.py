887a1d0af233aba754e1ea04e8738f5f67dc4f28
supertommc <correiodotomas@gmail.com>
2021-03-01 15:16:03 +0000

    Started minimax and UI

---
+++ graphics.py
@@ -0,0 +1,84 @@
+from p5 import *
+
+game =  [
+            [0, 2, 0, 2, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 2, 0, 0],
+            [0, 1, 0, 1, 0]
+        ]
+
+
+
+class Button:
+    def __init__(self, position, width, height, color, text):
+        self.x, self.y = position
+        self.color = color
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def display(self):
+        offset = 0
+        if self.is_hover():
+            offset = 30
+        fill(self.color[0]+offset, self.color[1]+offset, self.color[2]+offset)
+        rect(self.x, self.y, self.width, self.height)
+        fill(0, 0, 0)
+        text(self.text, self.x + 40, self.y+self.height/2-1)
+
+    def is_hover(self):
+        return mouse_x > self.x and mouse_x < self.x + self.width and mouse_y > self.y and mouse_y < self.y + self.height
+
+
+def draw_menu():
+    button = Button((100, 100), 100, 50, (100, 100, 100), "Play")
+    button.display()
+
+
+def draw_board(game):
+    # Vertical Lines
+    line((100, 100), (100, 600))
+    line((200, 100), (200, 600))
+    line((300, 100), (300, 600))
+    line((400, 100), (400, 600))
+    line((500, 100), (500, 600))
+    line((600, 100), (600, 600))
+
+    # Horizontal Lines
+    line((100, 100), (600, 100))
+    line((100, 200), (600, 200))
+    line((100, 300), (600, 300))
+    line((100, 400), (600, 400))
+    line((100, 500), (600, 500))
+    line((100, 600), (600, 600))
+
+    draw_state(game)
+
+def draw_piece(piece, position):
+    if (piece == 1):
+        fill(0, 0, 0)
+        circle(position, 50)
+    elif piece == 2:
+        fill(255, 255, 255)
+        circle(position, 50)
+
+
+def draw_state(game):
+    for i in range(len(game)):
+        for j in range(len(game[0])):
+            if game[i][j] != 0:
+                draw_piece(game[i][j], (550-j*100, 150+i*100))
+
+
+def setup():
+    size(700, 700)
+
+def draw():
+    #draw_board(game)
+    draw_menu()
+
+
+run()
+
+
--- neutreeko.py
+++ neutreeko.py
@@ -12,6 +12,25 @@ game =  [
             [0, 1, 0, 1, 0]
         ]
 
+"""
+
+    peças no centro
+    peças juntas sem estarem cercadas
+    peças do adversario no meio das tuas peças
+    casas que tu controlas e o adverario nao
+
+
+"""
+
+"""
+
+                        1
+            
+            x       y       z       w
+        c v         
+
+
+"""
 
 
 def valid_coord(coord):
@@ -104,6 +123,14 @@ def generate_moves(game, piece_pos):
 
     return moves
 
+def generate_all_moves(game, piece):
+    moves = []
+    for i in range(len(game)):
+        for j in range(len(game[0])):
+            if game[i][j] == piece:
+                moves.extend(generate_moves(game, (j, i)))
+    return moves
+
 def make_move(game, move):
     x, y, new_x, new_y = move
     piece = game[y][x]
@@ -113,6 +140,18 @@ def make_move(game, move):
     
     game[y][x] = 0
     game[new_y][new_x] = piece
+
+    return True
+
+def unmake_move(game, move):
+    x, y, new_x, new_y = move
+    piece = game[new_y][new_x]
+
+    if piece == 0 or game[y][x] != 0:
+        return False
+    
+    game[new_y][new_x] = 0
+    game[y][x] = piece
 
     return True
 
@@ -144,10 +183,93 @@ def check_game_over(game, piece):
 
     return res != 0
 
+def check_game_over_full(game):
+    if check_game_over(game, 1):
+        return 1
+    elif check_game_over(game, 2):
+        return 2
+    else:
+        return 0
+
+def get_other_piece(piece):
+    return 1 if piece == 2 else 2
+
+def evaluate_position(piece, game):
+    if check_game_over(game, piece):
+        return 1
+    else:
+        return 0
+    
+
+def minimax(is_max, current_player, game, depth):
+
+    print(depth)
+    print(is_max)
+
+    res = check_game_over_full(game)
+    print("Res: " + str(res))
+    print("Current Player: " + str(current_player))
+    if res != 0:
+        print("Game Over!!")
+        if res != current_player:
+            return 1
+        else:
+            return -1
+
+    if depth == 0:
+        return 0
+    
+    pos_scores = []
+    moves = generate_all_moves(game, current_player)
+    for move in moves:
+        print("Move: ")
+        print(move)
+        make_move(game, move)
+
+        pprint(game)
+
+        score = minimax(not is_max, get_other_piece(current_player), game, depth-1)
+        print("Score of depth {}: {}".format(depth, score))
+        print()
+        pos_scores.append(score)
+
+        unmake_move(game, move)
+
+    if is_max:
+        return max(pos_scores)
+    else:
+        return min(pos_scores)
+    
+"""
+    if is_max:
+        return max(scores)
+    else return min(scores)
+"""
 
 
-print(game_to_int(game, 1))
-print(check_game_over(game_, 2))
+game_ =  [
+            [0, 2, 0, 2, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 2, 0, 0],
+            [0, 1, 0, 1, 1]
+        ]
+
+print("Minimax: " + str(minimax(True, 1, game_, 3)))
+
+
+"""
+pprint(game_)
+make_move(game_, (1, 0, 0, 1))
+pprint(game_)
+unmake_move(game_, (1, 0, 0, 1))
+pprint(game_)
+"""
+
+#print(generate_all_moves(game_, 2))
+
+#print(bin(game_to_int(game, 1)))
+#print(check_game_over(game_, 2))
 
 """
 moves = generate_moves(game, (2, 1))
