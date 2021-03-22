43ce42154e6219253f50a7d67697512b19721d30
supertommc <correiodotomas@gmail.com>
2021-03-22 12:11:40 +0000

    Openings improvement

--- openings_book.py
+++ openings_book.py
@@ -1,5 +1,6 @@
 
 from collections import deque
+import re
 
 class OpeningsNode:
     def __init__(self, depth, move, parent):
@@ -7,6 +8,7 @@ class OpeningsNode:
         self.move = move
         self.parent = parent
         self.children = []
+        self.winner = 0
 
     def addChildNode(self, child):
         self.children.append(child)
@@ -14,6 +16,9 @@ class OpeningsNode:
 class OpeningsTree:
     def __init__(self):
         self.root = OpeningsNode(0, (0, 0, 0, 0), None)
+        self.res_list = []
+        self.res_list_offset = 0
+        self.count = 0
 
     def bfsAddChild(self, child_move, target_move, target_depth):
         queue = deque()
@@ -50,35 +55,82 @@ class OpeningsTree:
 
     def dfsAuxPrint(self, node):
         
-        print("\t"*(node.depth-1) + " " + str(node.move))
+        print("\t"*(node.depth-1) + " " + str(node.move) + " " + str(node.depth) + " " + str(node.winner))
 
         for child in node.children:
             self.dfsAuxPrint(child)
 
-    def dfsFindMove(self, stored_moves):
+    def dfsFindMove(self, stored_moves, piece):
         node = self.root
 
-        return self.dfsFindMoveAux(node, stored_moves)
+        return self.dfsFindMoveAux(node, stored_moves, piece)
 
-    def dfsFindMoveAux(self, node, stored_moves):
+    def dfsFindMoveAux(self, node, stored_moves, piece):
 
         move = 0
-
-        print("Depth node: " + str(node.depth))
-        print("Len stored moves: " + str(len(stored_moves)))
-
+        
         if (node.depth == len(stored_moves)):
-            print("OLAAA")
-            print(node.move)
-            return node.move
+            candidate_move = 0
+            for child in node.children:
+                if child.winner == piece:
+                    return child.move
+                elif child.winner == 5 and candidate_move == 0:
+                    candidate_move = child.move
+            return candidate_move
         
         for child in node.children:
             if child.move != stored_moves[child.depth-1]:
                 continue
-            move = self.dfsFindMoveAux(child, stored_moves)
+            move = self.dfsFindMoveAux(child, stored_moves, piece)
 
         return move
+
+    def consume_res_list_item(self):
+        item = self.res_list[self.res_list_offset]
+        self.res_list_offset += 1
+        return item
+
+    def dfsCountLeaves(self):
+        self.dfsCountLeavesAux(self.root)
+        c = self.count
+        self.count = 0
+        return c
+
+    def dfsCountLeavesAux(self, node):
+        if len(node.children) == 0:
+            self.count += 1
+        for child in node.children:
+            self.dfsCountLeavesAux(child)
+
+    def dfsAddWinner(self):
+
+        node = self.root
         
+        self.dfsAddWinnerAux(node)
+
+    def dfsAddWinnerAux(self, node):
+
+        if len(node.children) == 0:
+            res = self.consume_res_list_item()
+            if res != 5:
+                if node.depth % 2 == 0:
+                    print("Node depth: " + str(node.depth))
+                    node.winner = 2
+                else:
+                    node.winner = 1
+            else:
+                node.winner = 5
+            return node.winner
+        else:
+            winner = 0
+            for child in node.children:
+                winner |= self.dfsAddWinnerAux(child)
+            if winner != 1 and winner != 2:
+                winner = 5
+            node.winner = winner
+            return node.winner
+
+
 
 
 class OpeningsBook:
@@ -134,14 +186,36 @@ class OpeningsBook:
                 child = OpeningsNode(move_depth, move, parent)
                 parent.addChildNode(child)
                 possible_parents.append(child)
-    
-    def find_next_move(self, stored_moves):
-        return self.tree.dfsFindMove(stored_moves)
-
         
+        self.loadDrawsAndNumMovesToWin(file_str)
+
+    def loadDrawsAndNumMovesToWin(self, openings_str):
+        lines = openings_str.splitlines()
+        res_list = []
+
+        pattern1 = "DRAW"
+        pattern2 = "<..>"
+
+        for line in lines:
+            if re.search(pattern1, line):
+                res_list.append(5)
+            elif re.search(pattern2, line):
+                res_list.append(1)
+        
+        print("Res_list size: " + str(len(res_list)))
+        self.tree.res_list = res_list
+        self.tree.dfsAddWinner()
+
+    
+    def find_next_move(self, stored_moves, piece):
+        return self.tree.dfsFindMove(stored_moves, piece)
+
+
 
 book = OpeningsBook()
 
 book.loadOpenings("openings.txt")
 
-print(book.find_next_move([(1, 4, 2, 4), (2, 3, 2, 2)]))
+#book.tree.dfsPrint()
+
+print(book.find_next_move([(1, 4, 2, 4), (2, 3, 2, 2)], 1))
